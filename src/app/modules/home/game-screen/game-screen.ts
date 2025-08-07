341784e05d5625293c1ca9e3105befce39d57feb
Brandon Ho <brandon@manticorp.co.za>
2025-08-07 19:22:19 +0100

    Added game over logic and game counter

--- src/app/modules/home/game-screen/game-screen.ts
+++ src/app/modules/home/game-screen/game-screen.ts
@@ -1,9 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { GameConfig, GameTurnRequest } from '../../../../api-client';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { Store } from '@ngrx/store';
 import { postTurn } from '../../../store/actions/game-controller.actions';
+import { TranslateService } from '@ngx-translate/core';
+import { GameResult } from '../../../shared/models/GameResult';
 
 @Component({
   selector: 'app-game-screen',
@@ -14,12 +16,28 @@ import { postTurn } from '../../../store/actions/game-controller.actions';
 export class GameScreen {
   @Input() set gameConfig(gc: GameConfig | null) {
     console.warn('GameConfig is set:', gc);
-    this.gameOptions = gc?.gameChoices || [];
-    this.numberOfGames = gc?.numberOfGames || 0;
+    if (gc) {
+      this.gameOptions = gc.gameChoices;
+      this.numberOfGames = gc.numberOfGames;
+      this.gameOver = false;
+    }
   }
+
+  @Input() set gameResults(results: GameResult[] | null) {
+    console.warn('Game results are set:', results);
+    this.gameNumber = results?.length ? results?.length + 1 : 1;
+
+    if (results?.length === this.numberOfGames) {
+      this.gameOver = true;
+    }
+  }
+
+  private translate = inject(TranslateService);
 
   gameOptions: GameConfig['gameChoices'] = [];
   numberOfGames: number = 0;
+  gameNumber: number = 0;
+  gameOver: boolean = false;
 
   constructor(private readonly store: Store) {}
 
@@ -28,7 +46,7 @@ export class GameScreen {
       postTurn({
         postTurnData: {
           requestBody: {
-            turnNumber: 1,
+            turnNumber: this.gameNumber,
             userSelection: gameChoice,
           },
         },
